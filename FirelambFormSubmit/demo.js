d2bcae0ac91fb23307fc0d77cb4a4be805d5d51a
firelamb <firelamb@qq.com>
2014-04-29 14:17:16 +0800

    core files
    core files

---
+++ FirelambFormSubmit/demo.js
@@ -0,0 +1,40 @@
+﻿$(document).ready(function () {
+    $("#btnajaxpost").FirelambFormSubmit({
+        formId: "#frmAjax"
+        , submitType: "ajaxPost"
+        ,ajaxTips:"正在处理数据，请稍后..."
+        , postUrl: "demo/ajax/a.ashx"
+        , isEncrypt: true
+        , encryptObjs: ["#ajaxpwd2"]
+        , encryptType: "RC4"
+        , callEvent: function () {
+            $("#ajaxpwd2show").html("I am the text from server side post back: "+$("#ajaxpwd2").val());
+            $("#ajaxpwd2show").show();
+        }
+    });
+
+    $("#btndirpost").FirelambFormSubmit({
+        formId: "#frmdir"
+       , submitType: "dirPost"   
+        , isEncrypt: true
+        , encryptObjs: ["#ajaxpwd4"]
+        , encryptType: "SHA1"
+        , callEvent: function () {
+            $("#ajaxpwd4show").html("I am the text from server side post back: " + $("#ajaxpwd4").val());
+            $("#ajaxpwd4show").show();
+        }
+    });
+    $("#btncallpost").FirelambFormSubmit({
+        formId: "#frmcall"
+      , submitType: "ajaxPost"
+       , postUrl: "demo/ajax/a.ashx"
+       , isEncrypt: true
+       , encryptObjs: ["#ajaxpwd5"]
+       , encryptType: "SHA1"
+       , callEvent: function () {
+           $("#ajaxpwd5show").html("CallEvent:I am the text from server side post back: " + $("#ajaxpwd5").val());
+           $("#ajaxpwd5show").show();
+       }
+    });
+    
+});
---
+++ FirelambFormSubmit/demo/ajax/a.ashx
@@ -0,0 +1,26 @@
+﻿<%@ WebHandler Language="C#" Class="a" %>
+
+using System;
+using System.Web;
+
+public class a : IHttpHandler {
+    
+    public void ProcessRequest (HttpContext context) {
+      // context.Response.ContentType = "application/json";
+      context.Response.ContentType = "text/plain";
+        var s = context.Request.Form["ajaxpwd2"];
+        //s = context.Request.QueryString["ajaxpwd2"];
+      //  s = context.Request.Params["ajaxpwd2"];
+        var ss = context.Request.Form["ajaxpwd1"];
+      //  ss = context.Request.Params["ajaxpwd1"].ToString();
+        context.Response.Write(s);
+        
+    }
+ 
+    public bool IsReusable {
+        get {
+            return false;
+        }
+    }
+
+}
